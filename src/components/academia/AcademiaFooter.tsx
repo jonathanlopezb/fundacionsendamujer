365632c8709739fc182bd514767e34bee53d764a
Jonathanlopezb <jonatanlopezbello@hotmail.com>
2026-09-18 22:52:20 -0500

    desarrollado

--- src/components/academia/AcademiaFooter.tsx
+++ src/components/academia/AcademiaFooter.tsx
@@ -25,6 +25,7 @@ export default function AcademiaFooter() {
         {/* Center credits */}
         <div className="flex items-center gap-2 text-center text-[11px]">
           <span>Dirección Ejecutiva: Dra. Sorelvis Murillo (+57 301 469 2095)</span>
+          <span>Diseñado y desarrollado: Jonathan L.</span>
         </div>
 
         {/* Back to main site link */}
